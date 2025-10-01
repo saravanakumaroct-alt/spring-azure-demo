a0470212bb7fc6acd9db3e0a48f3260c994ff550
Saravanakumar Subramanian <saravanakumaroct@gmail.com>
2025-10-01 20:29:03 +0530

    first commit

---
+++ src/main/java/com/example/spring_azure_demo/SpringAzureDemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.spring_azure_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String message(){
+		return "application deployed successfully";
+	}
+}
